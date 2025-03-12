74fcde8f8a279797296786a2a55a3f488db84415
Ilram Lima <ilramferreira@hotmail.com>
2025-03-12 12:40:44 -0300

    Criar projeto DAO com java

---
+++ src/db/BDIntegrityException.java
@@ -0,0 +1,13 @@
+package db;
+
+public class BDIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public BDIntegrityException(String msg) {
+		super(msg);
+	}
+
+	
+	
+}
