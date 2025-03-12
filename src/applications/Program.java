98713d81b279903cbb0902865277a4909ef19be9
Ilram Lima <ilramferreira@hotmail.com>
2025-03-12 16:02:20 -0300

    Classe Department

---
+++ src/applications/Program.java
@@ -0,0 +1,15 @@
+package applications;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+		
+	}
+
+}
