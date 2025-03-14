32be5e8184d9f0f1562af5680bac2160e6498399
Ilram Lima <ilramferreira@hotmail.com>
2025-03-14 12:41:32 -0300

    Criação do DepartmentDaoJDBC, SellerDaoJDBC e DaoFactory

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao{
+
+	@Override
+	public void insert(Department obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Department findId(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		
+		return null;
+	}
+
+}
